5c7211daae3a22d9e208329d8634d2420c2c8c8d
dimplejha <dimple.jha@olacabs.com>
2023-06-23 16:00:03 +0530

    feat: updated user schema
    Signed-off-by: dimplejha <dimple.jha@olacabs.com>

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,50 +1,60 @@
+const { Timestamp } = require('mongodb');
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-    enum: ['Mr', 'Mrs', 'Miss'],
+const userSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    DOB: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: Date,
+    updatedAt: Date,
   },
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-    trim: true,
-    enum: ['Male', 'Female', 'Other'],
-  },
-  DOB: {
-    type: Date,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  createdAt: Date,
-  updatedAt: Date,
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('users', userSchema);
